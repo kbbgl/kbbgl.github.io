3325131d680639d13083969209218f2b96dd1171
kbbgl <kobbi.gal@gmail.com>
2025-01-31 01:35:03 +0000

    Updates

---
+++ assets/js/5180e398.15859381.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkkgkb=self.webpackChunkkgkb||[]).push([[96022],{45499:a=>{a.exports=JSON.parse('{"tag":{"label":"storage","permalink":"/blog/tags/storage","allTagsPath":"/blog/tags","count":1,"unlisted":false},"listMetadata":{"permalink":"/blog/tags/storage","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
