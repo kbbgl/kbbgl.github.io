bccff5ac6328180697dece6f45a58d94ef7af176
kbbgl <kobbi.gal@gmail.com>
2025-10-19 18:08:31 +0000

    Updates

---
+++ assets/js/b409eef7.515a08a0.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkkgkb=self.webpackChunkkgkb||[]).push([[73159],{66909:t=>{t.exports=JSON.parse('{"tag":{"label":"route","permalink":"/blog/tags/route","allTagsPath":"/blog/tags","count":1,"unlisted":false},"listMetadata":{"permalink":"/blog/tags/route","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ assets/js/fe02ca2e.965e397c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkkgkb=self.webpackChunkkgkb||[]).push([[14025],{52203:t=>{t.exports=JSON.parse('{"tag":{"label":"routing","permalink":"/blog/tags/routing","allTagsPath":"/blog/tags","count":1,"unlisted":false},"listMetadata":{"permalink":"/blog/tags/routing","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
