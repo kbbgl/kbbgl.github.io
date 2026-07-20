e41fa66c75dedc423268b86cbb704aba7e0ea0b7
kbbgl <kobbi.gal@gmail.com>
2026-07-20 22:17:17 +0000

    Updates

---
+++ assets/js/b94bf011.428c6dff.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkkgkb=globalThis.webpackChunkkgkb||[]).push([[18946],{98788:a=>{a.exports=JSON.parse('{"tag":{"label":"go","permalink":"/blog/tags/go","allTagsPath":"/blog/tags","count":1,"unlisted":false},"listMetadata":{"permalink":"/blog/tags/go","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
